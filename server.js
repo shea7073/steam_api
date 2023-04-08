4b4e6dc5cd6e4385c8a200b71d73d926fd548f3b
shea7073 <shea7073@gmail.com>
2023-04-08 14:35:39 -0400

    header fix

--- server.js
+++ server.js
@@ -9,9 +9,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 
-let routes = require('./routes/index');4
+let routes = require('./routes/index');
 app.use('/', routes);
-app.use(cors);
+app.use(cors());
 
 app.use(function(req, res) {
     res.status(404);
