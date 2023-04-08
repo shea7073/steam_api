b065d1c92566a86c0928d68ac3c14bdd0e14b9d7
shea7073 <shea7073@gmail.com>
2023-04-08 14:28:14 -0400

    cors

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors')
 
 const app = express();
 const PORT = process.env.PORT || 3030;
@@ -10,6 +11,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 let routes = require('./routes/index');4
 app.use('/', routes);
+app.use(cors);
 
 app.use(function(req, res) {
     res.status(404);
